bf3be3594fad8cdab4da4e37ee589b59e769778b
AnjanaSenanayake <anjanajayabandara@gmail.com>
2021-03-22 21:09:49 +0530

    npy export & local configs added

--- fast5_read.py
+++ fast5_read.py
@@ -4,38 +4,46 @@ import numpy as np
 from main import load_model_values, ref_reads, call_raw_values
 from fast5_research import Fast5
 from random import randrange
+from LocalConfigs import *
 
-NANOPORE_MODEL = "nanopore_model.txt"
-REF_FILE = "/storage/e14317/covid/nCov-2019-ref.fasta"
-PAF_FILE = "/storage/e14317/covid/out.paf"
-DATASET = "/storage/e14317/covid/single_fast5/"
-#DATASET = "/storage/e14317/zymo/Zymo-GridION-EVEN-BB-SN/GA10000/reads/"
-OUTPUT_FILE = "/storage/e14317/covid/train_data.csv"
-#OUTPUT_FILE = "/storage/e14317/zymo/Zymo-GridION-EVEN-BB-SN/train_data.csv"
 BASE_SAMPLING_RATE = 12
 DIGITIZATION = 8192
 RANGE = 1402.882
 
+fast5_file_count = 0
+np_file_count = 0
+
 
 def print_all_raw_data(fast5_file):
+    np_array = None
+    global fast5_file_count
+    global np_file_count
     with get_fast5_file(fast5_file, mode="r") as f5:
+        fast5_file_count = fast5_file_count + 1
         with Fast5(fast5_file) as fh5:
             for read in f5.get_reads():
-                #with open(PAF_FILE) as paf:
-                    raw_data = read.get_raw_data()
-                    print(read.read_id)
-                    #paf_line = line_that_contain(read.read_id, paf)
-                    #paf_line_tokens = paf_line.split()
+                # with open(PAF_FILE) as paf:
+                raw_data = read.get_raw_data()
+                # print(read.read_id)
+                # paf_line = line_that_contain(read.read_id, paf)
+                # paf_line_tokens = paf_line.split()
 
-                    rand_seed = len(raw_data) - 500
+                rand_seed = len(raw_data) - 500
 
-                    for i in range(8):
-                        start_idx = randrange(rand_seed)
-                        read_segment = raw_data[start_idx:start_idx+500]
-                        read_segment_converted = convert_to_pico(read_segment, fh5.channel_meta['offset'])
-                        print_line = ','.join(map(str, read_segment_converted)) + ',0\n'
-                        # print(print_line)
-                        raw_reads.write(print_line)
+                for i in range(8):
+                    start_idx = randrange(rand_seed)
+                    read_segment = raw_data[start_idx:start_idx + 500]
+                    read_segment_converted = convert_to_pico(read_segment, fh5.channel_meta['offset'])
+                    np_array = np.asarray(read_segment_converted, dtype=np.float32)
+                    np_array = np.append(np_array, 1)
+                    # print_line = ','.join(map(str, read_segment_converted)) + ',0\n'
+                    # print(print_line)
+                    # raw_reads.write(print_line)
+
+    if fast5_file_count == 1000:
+        fast5_file_count = 0
+        np_file_count = np_file_count + 1
+        np.save(OUTPUT_NPY_DIR + str(np_file_count), np_array)
 
 
 def line_that_contain(string, fp):
@@ -74,4 +82,6 @@ if __name__ == '__main__':
                     files_count = files_count + 1
                     files_per = str(files_count) + '/' + len_dirs
                 print(str(file_count) + '/' + str(len(files)) + ' | ' + files_per)
+                if files_count == 10:
+                    break
     raw_reads.close()
