b9f86c0c1401828f476967ce96711451af3d6529
AnjanaSenanayake <anjanajayabandara@gmail.com>
2022-07-25 22:37:04 +0530

    Update baseline.sh script new approach

--- support/baseline.sh
+++ support/baseline.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
-POS_FAST5_PATH=$1
-NEG_FAST5_PATH=$2
-POS_FASTQ_PATH=$3
-NEG_FASTQ_PATH=$4
-MIXED_REF=$5
-BASE_LENGTH=$6
+export slow5tools=$HOME/DeepSelectNet/tools/slow5tools-v0.3.0/slow5tools
+export minimap2=$HOME/DeepSelectNet/tools/minimap2/minimap2
+
+POS_SLOW5=$1
+NEG_SLOW5=$2
+POS_FASTQ=$3
+NEG_FASTQ=$4
+POS_REF=$5
+NEG_REF=$6
+BASE_LENGTH=$7
 POS_READ_IDS='read-ids-pos.txt'
 NEG_READ_IDS='read-ids-neg.txt'
-TESTFASTQ='testset'$BASE_LENGTH'.fastq'
-MAPPED_NAME='mapped'$BASE_LENGTH'.paf'
-MAPPED_POS='mapped-pos'$BASE_LENGTH'.txt'
-MAPPED_NEG='mapped-neg'$BASE_LENGTH'.txt'
+MIXED_REF='mixed-ref.fasta'
+POS_CONTIGS='pos-contigs.list'
+NEG_CONTIGS='neg-contigs.list'
+TMP_POS_FASTQ='pos-fastq.fastq'
+TMP_NEG_FASTQ='neg-fastq.fastq'
+MAPPED_POS='mapped-pos'$BASE_LENGTH'.paf'
+MAPPED_NEG='mapped-neg'$BASE_LENGTH'.paf'
 TRUE_POS='true-pos'$BASE_LENGTH'.txt'
 TRUE_NEG='true-neg'$BASE_LENGTH'.txt'
+FALSE_POS='false-pos'$BASE_LENGTH'.txt'
+FALSE_NEG='false-neg'$BASE_LENGTH'.txt'
 
+if [ ! -x $slow5tools ]; then
+  echo "slow5tools does not exists in provided directory. Please provide correct path as -> export slow5tools=<path_slow5tools>" && exit
+fi
+
+if [ ! -x "$minimap2" ]; then
+  echo "minimap2 does not exists in provided directory. Please provide correct path as -> export minimap2=<path_minimap2>" && exit
+fi
 
 if test -f "$POS_READ_IDS"; then
   echo "$POS_READ_IDS already exists in directory."
 else
   echo "Extracting pos read ids."
-  ../tools/slow5tools-v0.3.0/slow5tools view $POS_FAST5_PATH | grep -v '^[#@]' | awk '{print $1}' > $POS_READ_IDS
+  $slow5tools view $POS_SLOW5 | grep -v '^[#@]' | awk '{print $1}' > $POS_READ_IDS
   #python ~/DeepSelectNet/support/export_read_id.py -f5 $POS_FAST5_PATH -o $POS_READ_IDS
 fi
 
@@ -28,51 +44,66 @@ if test -f "$NEG_READ_IDS"; then
   echo "$NEG_READ_IDS already exists in directory."
 else
   echo "Extracting neg read ids."
-  ../tools/slow5tools-v0.3.0/slow5tools view $NEG_FAST5_PATH | grep -v '^[#@]' | awk '{print $1}' > $NEG_READ_IDS
+  $slow5tools view $NEG_SLOW5 | grep -v '^[#@]' | awk '{print $1}' > $NEG_READ_IDS
   #python ~/DeepSelectNet/support/export_read_id.py -f5 $NEG_FAST5_PATH -o $NEG_READ_IDS
 fi
 
-cat $POS_FASTQ_PATH $NEG_FASTQ_PATH > $TESTFASTQ
-python support/fastq_parser.py -fq $TESTFASTQ -shrt True
+cat $POS_REF $NEG_REF > $MIXED_REF
 
-
-if test -f $MAPPED_NAME;then
-  echo "Minimap is already completed."
+if test -z "$BASE_LENGTH"; then
+  echo "Using full read for minimap2"
+  cat $POS_FASTQ > $TMP_POS_FASTQ
+  cat $NEG_FASTQ > $TMP_NEG_FASTQ
 else
-  echo "Minimap is starting..."
-  ../tools/minimap2/minimap2 -cx map-ont --secondary=no $MIXED_REF $TESTFASTQ > $MAPPED_NAME
-  echo "Minimap is completed."
+  echo "Using $BASE_LENGTH bases read for minimap2"
+  awk -v N="${BASE_LENGTH}" '{if(NR%2==1){print $0} else{print substr($1,1,N)}}' $POS_FASTQ > $TMP_POS_FASTQ
+  awk -v N="${BASE_LENGTH}" '{if(NR%2==1){print $0} else{print substr($1,1,N)}}' $NEG_FASTQ > $TMP_NEG_FASTQ
 fi
-
-
-
-cat $MAPPED_NAME | awk '{if($6!~/N/) print $1}' | sort -u > $MAPPED_POS
-cat $MAPPED_NAME | awk '{if($6~/N/) print $1}' | sort -u > $MAPPED_NEG
-
-POS_TOTAL=$(cat $MAPPED_POS| wc -l)
-NEG_TOTAL=$(cat $MAPPED_NEG| wc -l)
-
 
 if test -f $MAPPED_POS;then
-   grep -Ff $MAPPED_POS $POS_READ_IDS > $TRUE_POS
+  echo "Minimap for positive set is already completed."
 else
-  echo "Skipping accuracy calculations since positive read filtering has failed."
+  echo "Minimap for positive set is starting..."
+  $minimap2 -cx map-ont --secondary=no $MIXED_REF $TMP_POS_FASTQ > $MAPPED_POS
+  echo "Minimap for positive set is completed."
 fi
-
 
 if test -f $MAPPED_NEG;then
-   grep -Ff $MAPPED_NEG $NEG_READ_IDS > $TRUE_NEG
+  echo "Minimap for negative set is already completed."
 else
-  echo "Skipping accuracy calculations since negative read filtering has failed."
+  echo "Minimap for negative set is starting..."
+  $minimap2 -cx map-ont --secondary=no $MIXED_REF $TMP_NEG_FASTQ > $MAPPED_NEG
+  echo "Minimap for negative set is completed."
 fi
+
+cat $POS_REF | grep "^>" | awk '{print $1}' | cut -c 2- | sort -u > $POS_CONTIGS
+#cat $NEG_REF | grep "^>" | awk '{print $1}' | cut -c 2- | sort -u > $NEG_CONTIGS
+
+grep -w -F -f $POS_CONTIGS $MAPPED_POS | awk '{print $1}' > $TRUE_POS
+grep -w -F -f $NEG_CONTIGS $MAPPED_POS | awk '{print $1}' > $FALSE_POS
+grep -w -F -f $NEG_CONTIGS $MAPPED_NEG | awk '{print $1}' > $TRUE_NEG
+grep -w -F -f $POS_CONTIGS $MAPPED_NEG | awk '{print $1}' > $FALSE_NEG
 
 TP=$(cat $TRUE_POS | sort -u | wc -l)
 TN=$(cat $TRUE_NEG | sort -u | wc -l)
 
-FP=$(expr $POS_TOTAL - $TP)
-FN=$(expr $NEG_TOTAL - $TN)
+FP=$(cat $FALSE_POS | sort -u | wc -l)
+FN=$(cat $FALSE_NEG | sort -u | wc -l)
+
 
 echo "True positive reads: $TP"
 echo "False positive reads: $FP"
 echo "True negative reads: $TN"
 echo "False negative reads: $FN"
+
+rm $MIXED_REF
+rm $POS_CONTIGS
+#rm $NEG_CONTIGS
+rm $TMP_POS_FASTQ
+rm $TMP_NEG_FASTQ
+rm $MAPPED_POS
+rm $MAPPED_NEG
+rm $TRUE_POS
+rm $TRUE_NEG
+rm $FALSE_POS
+rm $FALSE_NEG
